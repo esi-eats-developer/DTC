0345cab226091ada7a6b005e8d672dab48943681
Hyorin Kim <68832208+hyoriinvic@users.noreply.github.com>
2021-04-07 14:09:45 +0900

    update

---
+++ pilly.kr/scripts/numscroller-1.0542b.js
@@ -0,0 +1,70 @@
+/**
+* jQuery scroroller Plugin 1.0
+*
+* http://www.tinywall.net/
+* 
+* Developers: Arun David, Boobalan
+* Copyright (c) 2014 
+*/
+(function($){
+    $(window).on("load",function(){
+        $(document).scrollzipInit();
+        $(document).rollerInit();
+    });
+    $(window).on("load", function(){
+        $('.numscroller').scrollzip({
+            showFunction    :   function() {
+                                    numberRoller($(this).attr('data-slno'));
+                                },
+            wholeVisible    :     true,
+        });
+    });
+    $.fn.scrollzipInit=function(){
+        $('body').prepend("<div style='position:fixed;top:0px;left:0px;width:0;height:0;' id='scrollzipPoint'></div>" );
+    };
+    $.fn.rollerInit=function(){
+        var i=0;
+        $('.numscroller').each(function() {
+            i++;
+           $(this).attr('data-slno',i); 
+           $(this).addClass("roller-title-number-"+i);
+           $(this).css("visibility", "visible"); 
+        });        
+    };
+    $.fn.scrollzip = function(options){
+        var settings = $.extend({
+            showFunction    : null,
+            hideFunction    : null,
+            showShift       : 0,
+            wholeVisible    : false,
+            hideShift       : 0,
+        }, options);
+        return this.each(function(i,obj){
+            $(this).addClass('scrollzip');
+            $(this).addClass('isShown');
+            settings.showFunction.call( this );
+            return this;
+        });
+    };
+    function numberRoller(slno){
+            var min=$('.roller-title-number-'+slno).attr('data-min');
+            var max=$('.roller-title-number-'+slno).attr('data-max');
+            var timediff=$('.roller-title-number-'+slno).attr('data-delay');
+            var increment=$('.roller-title-number-'+slno).attr('data-increment');
+            var numdiff=max-min;
+            var timeout=(timediff*1000)/numdiff;
+            numberRoll(slno,min,max,increment,timeout);
+            
+    }
+    function numberRoll(slno,min,max,increment,timeout){
+    	min = parseInt(min);
+    	max = parseInt(max);
+        if(min<=max){
+            $('.roller-title-number-'+slno).html($.number(min));
+            min=parseInt(min)+parseInt(increment);
+            setTimeout(function(){numberRoll(eval(slno),eval(min),eval(max),eval(increment),eval(timeout))},timeout);
+        }else{
+            $('.roller-title-number-'+slno).html($.number(max));
+        }
+    }
+})(jQuery);
