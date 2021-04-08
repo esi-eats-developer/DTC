f192c1c400e162a56a34d0398832a104799f16a2
Hyorin Kim <68832208+hyoriinvic@users.noreply.github.com>
2021-04-08 15:47:56 +0900

    d

--- js/solution.js
+++ js/solution.js
@@ -3,18 +3,17 @@ function goEland(){
     var giftPrice = localStorage.getItem("price");
 
     if(giftCategory == "수면/스트레스"){giftCategory = "C1"}
-    if(giftCategory == "눈"){giftCategory = "C2"}
-    if(giftCategory == "두뇌 활동"){giftCategory = "C3"}
-    if(giftCategory == "피로/간"){giftCategory = "C4"}
-    if(giftCategory == "체지방/운동능력"){giftCategory = "C5"}
-    if(giftCategory == "피부 관리"){giftCategory = "C6"}
-    if(giftCategory == "뼈/관절"){giftCategory = "C7"}
-    if(giftCategory == "여성 건강(갱년기)"){giftCategory = "C8"}
-    if(giftCategory == "치아/잇몸"){giftCategory = "C9"}
+    else if(giftCategory == "눈"){giftCategory = "C2"}
+    else if(giftCategory == "두뇌 활동"){giftCategory = "C3"}
+    else if(giftCategory == "피로/간"){giftCategory = "C4"}
+    else if(giftCategory == "체지방/운동능력"){giftCategory = "C5"}
+    else if(giftCategory == "피부 관리"){giftCategory = "C6"}
+    else if(giftCategory == "뼈/관절"){giftCategory = "C7"}
+    else if(giftCategory == "여성 건강(갱년기)"){giftCategory = "C8"}
+    else {giftCategory = "C9"}
 
-    console.log(giftCategory);
-    console.log(giftPrice);
-    console.log(purchase[giftCategory][giftPrice]);
+    // console.log(giftCategory);
+    // console.log(giftPrice);
 
     if(giftPrice == "전체"){location.href = purchase[giftCategory]["T"];}
     if(giftPrice == "10만원 이상"){location.href = purchase[giftCategory]["H"];}
