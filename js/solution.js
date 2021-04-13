b09481f53fcc80f8e1397871ef10e8f9ab716bbd
esi-eats-developer <82066675+esi-eats-developer@users.noreply.github.com>
2021-04-13 16:30:51 +0900

    update!!

--- js/solution.js
+++ js/solution.js
@@ -1,6 +1,6 @@
 // 수면/스트레스
 var c1 = {
-    "products": "sample-img.jpg",
+    "products": "c-sample-img.jpg",
     "T" : "https://www.elandmall.com/search/search.action?kwd=%ED%83%80%ED%8A%B8%EC%B2%B4%EB%A6%AC",
     "H" : "like",
     "L" : "you"
@@ -8,7 +8,7 @@ var c1 = {
 
 // 눈
 var c2 = {
-    "products": "sample-img.jpg",
+    "products": "c-sample-img.jpg",
     "T" : "https://www.elandmall.com/search/search.action?kwd=%EB%88%88+%EA%B1%B4%EA%B0%95#hashPage1/kwd=%EB%88%88%20%EA%B1%B4%EA%B0%95&category_1depth=&category_2depth=1612309303&brand_nm=&brand_no=&vend_info=&size_info=&min_price=0&max_price=813000&min_rate=&max_rate=&color_info=&page_idx=1&pageSize=60&srchFd=null&sort=7&listType=image&applyStartDate=null&applyEndDate=null&dispStartDate=null&dispEndDate=null&newGoodsStartDate=null&newGoodsEndDate=null&mall_no=0000014&deliCostFreeYn=&filter_info=&quick_deli_poss_yn=&lot_deli_yn=&field_recev_poss_yn=&deliCostPoliNo=&price_chg_yn=N&",
     "H" : "",
     "L" : ""
@@ -16,7 +16,7 @@ var c2 = {
 
 // 두뇌 활동
 var c3 = {
-    "products": "sample-img.jpg",
+    "products": "c-sample-img.jpg",
     "T" : "https://www.elandmall.com/search/search.action?kwd=%EB%91%90%EB%87%8C#hashPage1/kwd=%EB%91%90%EB%87%8C&category_1depth=&category_2depth=1612309303&brand_nm=&brand_no=&vend_info=&size_info=&min_price=1000&max_price=297000&min_rate=&max_rate=&color_info=&page_idx=1&pageSize=60&srchFd=null&sort=7&listType=image&applyStartDate=null&applyEndDate=null&dispStartDate=null&dispEndDate=null&newGoodsStartDate=null&newGoodsEndDate=null&mall_no=0000014&deliCostFreeYn=&filter_info=&quick_deli_poss_yn=&lot_deli_yn=&field_recev_poss_yn=&deliCostPoliNo=&price_chg_yn=N&",
     "H" : "",
     "L" : ""
@@ -24,7 +24,7 @@ var c3 = {
 
 // 피로/간
 var c4 = {
-    "products": "sample-img.jpg",
+    "products": "c-sample-img.jpg",
     "T" : "https://www.elandmall.com/search/search.action?kwd=%ED%94%BC%EB%A1%9C#hashPage1/kwd=%ED%94%BC%EB%A1%9C&category_1depth=&category_2depth=1612309303&brand_nm=&brand_no=&vend_info=&size_info=&min_price=1000&max_price=532000&min_rate=&max_rate=&color_info=&page_idx=1&pageSize=60&srchFd=null&sort=1&listType=image&applyStartDate=null&applyEndDate=null&dispStartDate=null&dispEndDate=null&newGoodsStartDate=null&newGoodsEndDate=null&mall_no=0000014&deliCostFreeYn=&filter_info=&quick_deli_poss_yn=&lot_deli_yn=&field_recev_poss_yn=&deliCostPoliNo=&price_chg_yn=N&",
     "H" : "",
     "L" : ""
@@ -32,7 +32,7 @@ var c4 = {
 
 // 체지방/운동능력
 var c5 = {
-    "products": "sample-img.jpg",
+    "products": "c-sample-img.jpg",
     "T" : "https://www.elandmall.com/search/search.action?kwd=%EC%B2%B4%EC%A7%80%EB%B0%A9#hashPage1/kwd=%EC%B2%B4%EC%A7%80%EB%B0%A9&category_1depth=&category_2depth=1612309303&brand_nm=&brand_no=&vend_info=&size_info=&min_price=4000&max_price=265000&min_rate=&max_rate=&color_info=&page_idx=1&pageSize=60&srchFd=null&sort=7&listType=image&applyStartDate=null&applyEndDate=null&dispStartDate=null&dispEndDate=null&newGoodsStartDate=null&newGoodsEndDate=null&mall_no=0000014&deliCostFreeYn=&filter_info=&quick_deli_poss_yn=&lot_deli_yn=&field_recev_poss_yn=&deliCostPoliNo=&price_chg_yn=N&",
     "H" : "",
     "L" : ""
@@ -40,7 +40,7 @@ var c5 = {
 
 // 피부 관리
 var c6 = {
-    "products": "sample-img.jpg",
+    "products": "c-sample-img.jpg",
     "T" : "https://www.qualqual.co.kr/q/EziP5zxT",
     "H" : "",
     "L" : ""
@@ -48,7 +48,7 @@ var c6 = {
 
 // 뼈/관절
 var c7 = {
-    "products": "sample-img.jpg",
+    "products": "c-sample-img.jpg",
     "T" : "https://www.elandmall.com/search/search.action?kwd=%EB%BC%88+%EA%B4%80%EC%A0%88#hashPage1/kwd=%EB%BC%88%20%EA%B4%80%EC%A0%88&category_1depth=&category_2depth=1612309303&brand_nm=&brand_no=&vend_info=&size_info=&min_price=3000&max_price=134000&min_rate=&max_rate=&color_info=&page_idx=1&pageSize=60&srchFd=null&sort=7&listType=image&applyStartDate=null&applyEndDate=null&dispStartDate=null&dispEndDate=null&newGoodsStartDate=null&newGoodsEndDate=null&mall_no=0000014&deliCostFreeYn=&filter_info=&quick_deli_poss_yn=&lot_deli_yn=&field_recev_poss_yn=&deliCostPoliNo=&price_chg_yn=N&",
     "H" : "",
     "L" : ""
@@ -56,7 +56,7 @@ var c7 = {
 
 // 여성 건강(갱년기)
 var c8 = {
-    "products": "sample-img.jpg",
+    "products": "c-sample-img.jpg",
     "T" : "https://www.elandmall.com/search/search.action?kwd=%EA%B0%B1%EB%85%84%EA%B8%B0#hashPage1/kwd=%EA%B0%B1%EB%85%84%EA%B8%B0&category_1depth=&category_2depth=1612309303&brand_nm=&brand_no=&vend_info=&size_info=&min_price=12000&max_price=1500000&min_rate=&max_rate=&color_info=&page_idx=1&pageSize=60&srchFd=null&sort=7&listType=image&applyStartDate=null&applyEndDate=null&dispStartDate=null&dispEndDate=null&newGoodsStartDate=null&newGoodsEndDate=null&mall_no=0000014&deliCostFreeYn=&filter_info=&quick_deli_poss_yn=&lot_deli_yn=&field_recev_poss_yn=&deliCostPoliNo=&price_chg_yn=N&",
     "H" : "",
     "L" : ""
@@ -64,7 +64,7 @@ var c8 = {
 
 // 면역/항산화
 var c9 = {
-    "products": "sample-img.jpg",
+    "products": "c-sample-img.jpg",
     "T" : "https://www.elandmall.com/search/search.action?kwd=%EC%B9%98%EC%95%84+%EA%B1%B4%EA%B0%95#hashPage1/kwd=%EC%B9%98%EC%95%84%20%EA%B1%B4%EA%B0%95&category_1depth=&category_2depth=1612309303&brand_nm=&brand_no=&vend_info=&size_info=&min_price=1000&max_price=360000&min_rate=&max_rate=&color_info=&page_idx=1&pageSize=60&srchFd=null&sort=7&listType=image&applyStartDate=null&applyEndDate=null&dispStartDate=null&dispEndDate=null&newGoodsStartDate=null&newGoodsEndDate=null&mall_no=0000014&deliCostFreeYn=&filter_info=&quick_deli_poss_yn=&lot_deli_yn=&field_recev_poss_yn=&deliCostPoliNo=&price_chg_yn=N&",
     "H" : "",
     "L" : ""
